03a8ed2d66d444f53fafea41d2cc30629ca27d78
Владимир <64016822+RSvlad@users.noreply.github.com>
2026-08-20 11:16:20 +0200

    Updating

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Finance-project/",
+  base: "/Timska-kasa/",
   resolve: {
     alias: {
       "@identity": path.resolve(__dirname, "src/identity"),
